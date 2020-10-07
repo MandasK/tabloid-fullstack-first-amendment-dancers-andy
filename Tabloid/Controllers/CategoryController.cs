b72d2c8bf8ab21478daa951028712e4daa1943ec
MandasK <king.amanda.r@gmail.com>
2020-10-07 18:17:26 -0500

    List and Post working in API and React still need delete and edit

---
+++ Tabloid/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tabloid.Models;
+using Tabloid.Repositories;
+
+namespace Tabloid.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepo;
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepo = categoryRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_categoryRepo.GetAll());
+        }
+
+        [HttpPost]
+        public IActionResult Post(Category category)
+        {
+            _categoryRepo.AddCategory(category);
+            return CreatedAtAction(nameof(Get), new { id = category.Id }, category);
+        }
+           
+    }
+}
