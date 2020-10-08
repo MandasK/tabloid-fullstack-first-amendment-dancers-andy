6a883cf6bae9b244922e722e75b7ef48dc8be0a0
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-10-08 11:29:58 -0500

    displaying post list in react client

---
+++ Tabloid/client/src/components/PostList.js
@@ -0,0 +1,30 @@
+import React, { useContext, useEffect, useState, useRef } from "react";
+import { PostContext } from "../providers/PostProvider";
+import PostCard from "./PostCard";
+
+const PostList = () => {
+    const { posts, getAllPosts } = useContext(PostContext);
+
+
+    useEffect(() => {
+        getAllPosts();
+    }, []);
+
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="cards-column">
+                    {posts.map((post) => (
+                        <>
+                            <p>Title: {post.title}</p>
+                            <p>Author: {post.userProfile.firstName} {post.userProfile.lastName}</p>
+                            <p>Category: {post.category.name}</p>
+                        </>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PostList;
