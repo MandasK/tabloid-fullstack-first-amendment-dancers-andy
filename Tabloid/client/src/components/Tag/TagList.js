6c9a48e1897bad050016e59f214cbe2c283d0131
Patrick Murphy <engine_ear@mac.com>
2020-10-09 13:07:59 -0500

    List,edit, delete(with confirm) all happening on the same url route with conditional redering

--- Tabloid/client/src/components/Tag/TagList.js
+++ Tabloid/client/src/components/Tag/TagList.js
@@ -62,6 +62,96 @@ const TagList = (props) => {
     GetAllTags();
   };
 
+  const editDeleteView = () => {
+
+    if (tagToEdit !== undefined && !confirmView) {
+      return (
+        <>        
+          <div className="edit_Fields">
+            <h5 className="tag_Spacer">Edit tag '{tagToEdit.name}'</h5>
+            <div className="edit_Row1">
+              <Button className="tag_Button" color="primary">
+                <fieldset>
+                  <input
+                    type="text"
+                    size={tagToEdit.name.length}
+                    required
+                    onChange={(e) => handleFieldChange(e)}
+                    id="name"
+                    value={tagToEdit.name}
+                    placeholder={tagToEdit.name}
+                    className="edit_Tag_Input_Field"
+                  />
+                </fieldset>
+              </Button>
+              
+            </div>
+            <div className="edit_Row2">
+              <div className="left_side_buttons">
+              <Button
+                size="sm"
+                color="secondary"
+                className="tag_Action_Button"
+                onClick={DiscardTagChanges}
+              >
+                Discard
+              </Button>
+  
+              <Button
+              size="sm"
+                color="danger"
+                className="tag_Action_Button"
+                hidden={!save}
+                onClick={SaveTagChanges}
+              >
+                Save
+              </Button>
+              </div>
+              <div className="tag_Delete_Button">
+              
+                <Button
+                size="sm"
+                  color="danger"
+                  className="tag_Action_Button"
+                  onClick={() => RemoveTagConfirm(tagToEdit.id)}
+                >
+                  Delete
+                </Button>              
+              </div>
+            </div>
+          </div>         
+        </>
+      )
+    }
+
+    else if (tagToEdit !== undefined && confirmView) {
+      return (
+        
+        <div className="delete_Box">
+            <h5 className="delete_Tag_Headline">Are you sure you want to delete '{tagToEdit.name}'?</h5>
+          <div className="edit_Row1">
+            <Button
+              size="sm"
+              color="secondary"
+              className="tag_Action_Button"
+              onClick={CancelDelete}
+            >
+              Cancel
+            </Button>
+            <Button
+              size="sm"
+              color="danger"
+              className="tag_Action_Button"
+              onClick={() => RemoveTag(tagToEdit.id)}
+            >
+              Delete
+            </Button>
+          </div>                  
+        </div>
+      )
+    }
+  }
+
   return (
     <>
       <div className="tag_Headline">
@@ -70,80 +160,21 @@ const TagList = (props) => {
           Add New
         </Button>
       </div>
-      {tagToEdit !== undefined ? (
-        <div className="edit_Fields">
-          <h5 className="tag_Spacer">Edit tag '{tagToEdit.name}'</h5>
-          <div className="edit_Row1">
-            <Button className="tag_Button" color="primary">
-              <fieldset>
-                <input
-                  type="text"
-                  size={tagToEdit.name.length}
-                  required
-                  onChange={(e) => handleFieldChange(e)}
-                  id="name"
-                  value={tagToEdit.name}
-                  placeholder={tagToEdit.name}
-                  className="edit_Tag_Input_Field"
-                />
-              </fieldset>
-            </Button>
-            {!confirmView ? (
-              <Button
-                color="danger"
-                className="tag_Button"
-                onClick={() => RemoveTagConfirm(tagToEdit.id)}
-              >
-                Delete
-              </Button>
-            ) : (
-              <div className="deleteConfirm">
-                <div className="edit_Row1">
-                  <h5>Are you sure you want to delete '{tagToEdit.name}'?</h5>
-                  <Button
-                    color="secondary"
-                    className="tag_Button"
-                    onClick={CancelDelete}
-                  >
-                    Cancel
-                  </Button>
-                  <Button
-                    color="danger"
-                    className="tag_Button"
-                    onClick={() => RemoveTag(tagToEdit.id)}
-                  >
-                    Delete
-                  </Button>
-                </div>
-              </div>
-            )}
-          </div>
-          <div className="edit_Row2">
-            <Button
-              color="secondary"
-              className="tag_Button"
-              onClick={DiscardTagChanges}
-            >
-              Discard Changes
-            </Button>
-
-            <Button
-              color="danger"
-              className="tag_Button"
-              hidden={!save}
-              onClick={SaveTagChanges}
-            >
-              Save Changes
-            </Button>
+      <h5 className="tag_Spacer">Click to edit</h5>
+      <div className="tag_View_Container">
+        <div>
+          <div className="tag_Container">
+            {tags.map((tag) => (
+          <Tag key={tag.id} tag={tag} editTag={editTag} />
+          ))}
           </div>
         </div>
-      ) : (
-        <h5 className="tag_Spacer">Click to edit</h5>
-      )}
-      <div className="tag_Container">
-        {tags.map((tag) => (
-          <Tag key={tag.id} tag={tag} editTag={editTag} />
-        ))}
+              
+       
+      {editDeleteView()} 
+      
+          </div>
+        <div>
       </div>
     </>
   );
